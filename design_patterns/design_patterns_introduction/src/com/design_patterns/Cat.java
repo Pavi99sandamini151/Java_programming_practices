29807cffa43f64f607dbfba3aa375db74fc1f9f4
Pavithra Sandamini <84126210+Pavi99sandamini151@users.noreply.github.com>
2023-01-13 13:17:21 +0530

    design patterns introduction

---
+++ design_patterns/design_patterns_introduction/src/com/design_patterns/Cat.java
@@ -0,0 +1,10 @@
+package com.design_patterns;
+
+public class Cat extends Animal {
+
+	public Cat() {
+		super();
+		
+		setSound("Meow");
+	}
+}
---
+++ design_patterns/design_patterns_introduction/src/com/design_patterns/WorkWithAnimals.java
@@ -0,0 +1,13 @@
+package com.design_patterns;
+
+public class WorkWithAnimals {
+	public static void main(String[] args) {
+		Dog fido = new Dog();
+		fido.setName("Fido");
+		System.out.println(fido.getName());
+		
+		fido.digHole();
+		
+		fido.setWeight(-2);
+	}
+}

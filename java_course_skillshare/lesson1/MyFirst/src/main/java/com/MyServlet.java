817c736a7e93548399007364631c7d276e641e96
Pavithra Sandamini <84126210+Pavi99sandamini151@users.noreply.github.com>
2023-01-15 19:57:25 +0530

    skill share java course

---
+++ java_course_skillshare/lesson1/MyFirst/src/main/java/com/MyServlet.java
@@ -0,0 +1,57 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		PrintWriter out = response.getWriter();
+//		out.print("Hello World, Service method");
+//	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("fname");
+		out.print("Hello World, get method");
+		out.print("<br/> Hello" + name);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("fname");
+		out.print("Hello World, post method");
+		out.print("<br/> Hello" + name);
+	}
+
+}
